dfa78abfba65fd55da305dd32df097d15ab683a9
jonathanodonnell <jonathan.carl.odonnell@gmail.com>
2014-06-20 10:10:13 -0300

    refactored to hand_factory and EvaluateHand class

--- lib/win.rb
+++ lib/win.rb
@@ -1,3 +1,5 @@
+require_relative '../lib/evaluate_hand.rb' unless defined?(EvaluateHand)
+
 class Win
   attr_accessor :player1, :player2, :middle_cards
 
@@ -78,138 +80,29 @@ class Win
   end
 
   def hand_rank(cards)
-    #TODO TEST these lines and this method is too big
-    # cards = @player1.cards + @middle_cards if player == 'Player1'
-    # cards = @player2.cards + @middle_cards if player == 'Player2'
-
     index = 0
-    if is_royal_flush?(cards)
+    if EvaluateHand.is_royal_flush?(cards)
       index = 9
-    elsif is_straight_flush?(cards)
+    elsif EvaluateHand.is_straight_flush?(cards)
       index = 8
-    elsif is_four_of_a_kind?(cards)
+    elsif EvaluateHand.is_four_of_a_kind?(cards)
       index = 7
-    elsif is_full_house?(cards)
+    elsif EvaluateHand.is_full_house?(cards)
       index = 6
-    elsif is_flush?(cards).first
+    elsif EvaluateHand.is_flush?(cards).first
       index = 5
-    elsif is_straight?(cards)
+    elsif EvaluateHand.is_straight?(cards)
       index = 4
-    elsif is_three_of_a_kind?(cards)
+    elsif EvaluateHand.is_three_of_a_kind?(cards)
       index = 3
-    elsif is_two_pair?(cards)
+    elsif EvaluateHand.is_two_pair?(cards)
       index = 2
-    elsif is_one_pair?(cards)
+    elsif EvaluateHand.is_one_pair?(cards)
       index = 1
-    elsif is_high_card?(cards)
+    elsif EvaluateHand.is_high_card?(cards)
       index = 0
     end
 
     {hand_rank: index, hand_string: Win::HANDS[index], set_cards: true, other_cards: true}
-  end
-
-  def is_royal_flush?(cards)
-    is_flush, flush_cards, other_cards = is_flush?(cards)
-    is_straight = Sorter.is_high_straight?(flush_cards)
-    is_flush && is_straight
-  end
-
-  def is_straight_flush?(cards)
-    is_flush, flush_cards, other_cards = is_flush?(cards)
-    is_straight = Sorter.is_straight?(flush_cards)
-    is_flush && is_straight
-  end
-
-  def is_four_of_a_kind?(cards)
-    matched_desired_pairs?(cards, 4)[:result]
-  end
-
-  def is_full_house?(cards)
-    three_hash = matched_desired_pairs?(cards, 3)
-    two_hash = matched_desired_pairs?(cards, 2)
-    three_hash[:result] && two_hash[:result] && (three_hash[:card] != two_hash[:card])
-  end
-
-  def is_flush?(cards)
-    return_bool = !flush_suit(cards).nil?
-
-    return_cards = case flush_suit(cards)
-      when 'Hearts'
-        cards.select{|card| card.suit == 'Hearts'}
-      when 'Spades'
-        cards.select{|card| card.suit == 'Spades'}
-      when 'Diamonds'
-        cards.select{|card| card.suit == 'Diamonds'}
-      when 'Clubs'
-        cards.select{|card| card.suit == 'Clubs'}
-      else
-        []
-    end
-
-    other_cards = cards - return_cards
-    [return_bool, return_cards, other_cards]
-  end
-
-  def is_straight?(cards)
-    Sorter.is_straight?(cards)
-  end
-
-  def is_three_of_a_kind?(cards)
-    matched_desired_pairs?(cards, 3)[:result]
-  end
-
-  def is_two_pair?(cards)
-    first_pair = matched_desired_pairs?(cards, 2)
-    second_pair = matched_desired_pairs?(cards, 2, true)
-    different_pairs = first_pair[:card] != second_pair[:card]
-    first_pair[:result] && second_pair[:result] && different_pairs
-  end
-
-  def is_one_pair?(cards)
-    matched_desired_pairs?(cards, 2)[:result]
-  end
-
-  def is_high_card?(cards)
-    matched_desired_pairs?(cards, 1)[:result]
-  end
-
-  private
-
-  def matched_desired_pairs?(cards, desired_pairs, start_at_end=false)
-    cards = cards.map(&:number)
-    cards.reverse! if start_at_end
-    total_card_count = cards.count
-
-    pair_count = 0
-    value = 0
-    cards.each do |card|
-      unless pair_count == desired_pairs
-        pair_count = total_card_count - (cards - [card]).count
-        value = card
-      end
-    end
-    {result: pair_count == desired_pairs, card: value}
-  end
-
-  def flush_suit(cards)
-    suits = cards.map{|card| card.suit.downcase}
-    suits.sort!
-
-    hearts_count = suits.count('hearts')
-    clubs_count = suits.count('clubs')
-    diamonds_count = suits.count('diamonds')
-    spades_count = suits.count('spades')
-
-    if hearts_count >= 5
-      'Hearts'
-    elsif clubs_count >= 5
-      'Clubs'
-    elsif diamonds_count >= 5
-      'Diamonds'
-    elsif spades_count >= 5
-      'Spades'
-    else
-      nil
-    end
   end
 end
--- spec/win_spec.rb
+++ spec/win_spec.rb
@@ -1,4 +1,5 @@
 require 'win.rb' unless defined?(Win)
+require_relative '../lib/factories/hand_factory' unless defined?(HandFactory)
 
 describe Win do
   before(:each) do
@@ -7,42 +8,6 @@ describe Win do
     game.deal_middle_cards
     @win = Win.new(game.player1, game.player2, game.middle_cards)
     @player1_cards = @win.player1.cards + @win.middle_cards
-    @royal_flush_hand = [Card.new('A'), Card.new('K'),
-                          Card.new('Q'), Card.new('J'),
-                          Card.new('10')]
-
-    @straight_flush_hand = [Card.new('2'), Card.new('K'),
-                         Card.new('Q'), Card.new('J'),
-                         Card.new('10','Spades'), Card.new('9')]
-    
-    @high_card_hand = [Card.new('A'), Card.new('K'),
-                       Card.new('2'), Card.new('3'),
-                       Card.new('10','Hearts')]
-
-    @four_of_a_kind_hand = [Card.new('K'), Card.new('K'), Card.new('K'),
-                            Card.new('K'), Card.new('3'), Card.new('3'),
-                            Card.new('7')]
-
-    @full_house_hand = [Card.new('K'), Card.new('K'), Card.new('K'),
-                             Card.new('8'), Card.new('3'), Card.new('3'),
-                             Card.new('7')]
-
-    @flush_hand = [Card.new('K'), Card.new('A'), Card.new('K'), Card.new('8'),
-                             Card.new('4'), Card.new('3'), Card.new('7')]
-
-    @straight_hand = [Card.new('K'), Card.new('A', 'Clubs'), Card.new('Q', 'Hearts'),
-                      Card.new('J', 'Diamond'), Card.new('10'), Card.new('3'),
-                      Card.new('7', 'Hearts')]
-
-    @three_of_a_kind_hand = [Card.new('K', 'Clubs'), Card.new('K'), Card.new('K'),
-                             Card.new('8', 'Clubs'), Card.new('4'), Card.new('3'),
-                             Card.new('7', 'Hearts')]
-
-    @two_pair_hand = [Card.new('K', 'Clubs'), Card.new('K'), Card.new('Q'), Card.new('4'),
-                      Card.new('3', 'Hearts'), Card.new('4', 'Diamonds'), Card.new('7')]
-    
-    @one_pair_hand = [Card.new('K', 'Clubs'), Card.new('K'), Card.new('Q'), Card.new('10'),
-                      Card.new('3', 'Hearts'), Card.new('4', 'Diamonds'), Card.new('7')]
   end
 
   describe '#winner_string' do
@@ -60,118 +25,35 @@ describe Win do
     end
 
     it 'should return the correct index and hands' do
-      expect(@win.hand_rank(@royal_flush_hand)[:hand_rank]).to eq 9
-      expect(@win.hand_rank(@royal_flush_hand)[:hand_string]).to eq 'RoyalFlush'
+      expect(@win.hand_rank(HandFactory.royal_flush)[:hand_rank]).to eq 9
+      expect(@win.hand_rank(HandFactory.royal_flush)[:hand_string]).to eq 'RoyalFlush'
 
-      expect(@win.hand_rank(@straight_flush_hand)[:hand_rank]).to eq 8
-      expect(@win.hand_rank(@straight_flush_hand)[:hand_string]).to eq 'StraightFlush'
+      expect(@win.hand_rank(HandFactory.straight_flush)[:hand_rank]).to eq 8
+      expect(@win.hand_rank(HandFactory.straight_flush)[:hand_string]).to eq 'StraightFlush'
 
-      expect(@win.hand_rank(@four_of_a_kind_hand)[:hand_rank]).to eq 7
-      expect(@win.hand_rank(@four_of_a_kind_hand)[:hand_string]).to eq 'FourOfKind'
+      expect(@win.hand_rank(HandFactory.four_of_a_kind)[:hand_rank]).to eq 7
+      expect(@win.hand_rank(HandFactory.four_of_a_kind)[:hand_string]).to eq 'FourOfKind'
 
-      expect(@win.hand_rank(@full_house_hand)[:hand_rank]).to eq 6
-      expect(@win.hand_rank(@full_house_hand)[:hand_string]).to eq 'FullHouse'
+      expect(@win.hand_rank(HandFactory.full_house)[:hand_rank]).to eq 6
+      expect(@win.hand_rank(HandFactory.full_house)[:hand_string]).to eq 'FullHouse'
 
-      expect(@win.hand_rank(@flush_hand)[:hand_rank]).to eq 5
-      expect(@win.hand_rank(@flush_hand)[:hand_string]).to eq 'Flush'
+      expect(@win.hand_rank(HandFactory.flush)[:hand_rank]).to eq 5
+      expect(@win.hand_rank(HandFactory.flush)[:hand_string]).to eq 'Flush'
 
-      expect(@win.hand_rank(@straight_hand)[:hand_rank]).to eq 4
-      expect(@win.hand_rank(@straight_hand)[:hand_string]).to eq 'Straight'
+      expect(@win.hand_rank(HandFactory.straight)[:hand_rank]).to eq 4
+      expect(@win.hand_rank(HandFactory.straight)[:hand_string]).to eq 'Straight'
 
-      expect(@win.hand_rank(@three_of_a_kind_hand)[:hand_rank]).to eq 3
-      expect(@win.hand_rank(@three_of_a_kind_hand)[:hand_string]).to eq 'ThreeOfKind'
+      expect(@win.hand_rank(HandFactory.three_of_a_kind)[:hand_rank]).to eq 3
+      expect(@win.hand_rank(HandFactory.three_of_a_kind)[:hand_string]).to eq 'ThreeOfKind'
 
-      expect(@win.hand_rank(@two_pair_hand)[:hand_rank]).to eq 2
-      expect(@win.hand_rank(@two_pair_hand)[:hand_string]).to eq 'TwoPair'
+      expect(@win.hand_rank(HandFactory.two_pair)[:hand_rank]).to eq 2
+      expect(@win.hand_rank(HandFactory.two_pair)[:hand_string]).to eq 'TwoPair'
 
-      expect(@win.hand_rank(@one_pair_hand)[:hand_rank]).to eq 1
-      expect(@win.hand_rank(@one_pair_hand)[:hand_string]).to eq 'OnePair'
+      expect(@win.hand_rank(HandFactory.pair)[:hand_rank]).to eq 1
+      expect(@win.hand_rank(HandFactory.pair)[:hand_string]).to eq 'OnePair'
 
-      expect(@win.hand_rank(@high_card_hand)[:hand_rank]).to eq 0
-      expect(@win.hand_rank(@high_card_hand)[:hand_string]).to eq 'HighCard'
-    end
-  end
-
-  describe '#is_royal_flush?' do
-    it 'should only detect a royal flush' do
-      expect(@win.is_royal_flush?(@royal_flush_hand)).to eq true
-    end
-  end
-
-  describe '#is_four_of_a_kind' do
-    it 'should detect 4 of a kind' do
-      expect(@win.is_four_of_a_kind?(@four_of_a_kind_hand)).to eq true
-    end
-
-    it 'should not detect a full house' do
-      expect(@win.is_four_of_a_kind?(@full_house_hand)).to eq false
-    end
-  end
-
-  describe '#is_full_house' do
-    it 'should detect a full house' do
-      expect(@win.is_full_house?(@full_house_hand)).to eq true
-    end
-
-    it 'should not detect three of a kind' do
-      expect(@win.is_full_house?(@three_of_a_kind_hand)).to eq false
-    end
-  end
-
-  describe '#is_flush?' do
-    it 'should return true for a flush of cards' do
-      expect(@win.is_flush?(@flush_hand).first).to eq true
-      other_flush_hand = [Card.new('2'), Card.new('2', 'Clubs'),
-                          Card.new('9', 'Clubs'), Card.new('10', 'Clubs'),
-                          Card.new('7', 'Clubs'), Card.new('2', 'Hearts'),
-                          Card.new('K', 'Clubs')]
-      expect(@win.is_flush?(other_flush_hand).first).to eq true
-    end
-
-    it 'should return false for not a flush' do
-      expect(@win.is_flush?(@high_card_hand).first).to eq false
-    end
-  end
-
-  describe '#is_straight?' do
-    it 'should return true for a straight of cards' do
-      expect(@win.is_straight?(@straight_hand)).to eq true
-    end
-  end
-
-  describe '#is_three_of_a_kind' do
-    it 'should detect 3 of a kind' do
-      expect(@win.is_three_of_a_kind?(@three_of_a_kind_hand)).to eq true
-    end
-
-    it 'should not detect a non three of a kind' do
-      expect(@win.is_three_of_a_kind?(@high_card_hand)).to eq false
-    end
-  end
-
-  describe '#is_two_pair?' do
-    it 'should detect two pair' do
-      expect(@win.is_two_pair?(@two_pair_hand)).to eq true
-    end
-
-    it 'should not detect one pair' do
-      expect(@win.is_two_pair?(@one_pair_hand)).to eq false
-    end
-  end
-
-  describe '#is_one_pair?' do
-    it 'should detect a pair' do
-      expect(@win.is_one_pair?(@one_pair_hand)).to eq true
-    end
-
-    it 'should not detect a non pair' do
-      expect(@win.is_one_pair?(@high_card_hand)).to eq false
-    end
-  end
-
-  describe '#is_high_card?' do
-    it 'should detect a high card hand' do
-      expect(@win.is_high_card?(@high_card_hand)).to eq true
+      expect(@win.hand_rank(HandFactory.high_card)[:hand_rank]).to eq 0
+      expect(@win.hand_rank(HandFactory.high_card)[:hand_string]).to eq 'HighCard'
     end
   end
 end
