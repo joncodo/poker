7335d1c39b54f4394c57df630393d097455c7e8a
Jonathan O'Donnell <jonathan.carl.odonnell@gmail.com>
2015-07-21 14:39:15 -0400

    Update play.rb

--- play.rb
+++ play.rb
@@ -15,6 +15,8 @@ puts game.show_cards_string('Player1', game.player1.cards)
 puts ''
 puts game.show_cards_string('Player2', game.player2.cards)
 
+
+
 game.deal_middle_cards
 puts ''
 puts game.show_cards_string('Middle', game.middle_cards)
